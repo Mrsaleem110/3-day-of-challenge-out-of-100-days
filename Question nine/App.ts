3ffec05a3d9e7f2170ba5ef587ee56b67055e81f
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-04-26 15:29:29 +0500

    3day of challenge

---
+++ Question nine/App.ts
@@ -0,0 +1,3 @@
+// *Question 9:* Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+ let my_favourite_number : string="4";
+ console.log(`My favourite number is : ${my_favourite_number}`);
